90bac23939c23f0feb03084f046cdee6085b6d31
Maximiliano Kazanski <maxikazanski@gmail.com>
2025-11-21 19:29:26 -0300

    feat: update create script to send .tar

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# load environment variables
+set -a
+source .env
+set +a
+
 docker build -t one-piece-chapter-notifier .
 docker save -o one-piece-chapter-notifier.tar one-piece-chapter-notifier
+
+scp -P "$VPS_SSH_PORT" one-piece-chapter-notifier.tar "$VPS_USER@$VPS_IP:$VPS_PROJECT_ROUTE"
+
+rm -f one-piece-chapter-notifier.tar
+docker rmi one-piece-chapter-notifier
